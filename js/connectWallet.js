f15bda30cb665120cfaf11932137dcfde3b8f36d
Sholameister <info@VolunteerCoinProject.org>
2025-07-18 22:56:02 -0500

    Update connectWallet.js

--- js/connectWallet.js
+++ js/connectWallet.js
@@ -1,17 +1,27 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getFirestore, collection, addDoc, serverTimestamp } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCLLrOx4jWJ1PN8xFFxNhIryx3NshADKVY",
-  authDomain: "lovebutton-heaven.firebaseapp.com",
-  projectId: "lovebutton-heaven",
-  storageBucket: "lovebutton-heaven.firebasestorage.app",
-  messagingSenderId: "1079456151721",
-  appId: "1:1079456151721:web:15d2aa1171d977da8c11b8",
-  measurementId: "G-0261HYV08P"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+  import {getFirestore, collection, addDoc, serverTimestamp } from "firebase/firestore";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCLLrOx4jWJ1PN8xFFxNhIryx3NshADKVY",
+    authDomain: "lovebutton-heaven.firebaseapp.com",
+    projectId: "lovebutton-heaven",
+    storageBucket: "lovebutton-heaven.firebasestorage.app",
+    messagingSenderId: "1079456151721",
+    appId: "1:1079456151721:web:15d2aa1171d977da8c11b8",
+    measurementId: "G-0261HYV08P"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
 // ✅ Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
