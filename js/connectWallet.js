49dec95116d442a9a276c7a1b0e79861789cdcd3
Sholameister <info@VolunteerCoinProject.org>
2025-07-26 10:16:48 -0500

    Update connectWallet.js

--- js/connectWallet.js
+++ js/connectWallet.js
@@ -1,4 +1,16 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCLLrOx4jWJ1PN8xFFxNhIryx3NshADKVY",
+  authDomain: "lovebutton-heaven.firebaseapp.com",
+  projectId: "lovebutton-heaven",
+  storageBucket: "lovebutton-heaven.firebasestorage.app",
+  messagingSenderId: "1079456151721",
+  appId: "1:1079456151721:web:15d2aa1171d977da8c11b8",
+  measurementId: "G-0261HYV08P"
+};
+
 // Firebase
+firebase.initializingApp(firebaseConfig);
 const db = firebase.firestore();
 const storage = firebase.storage();
 
