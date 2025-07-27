5b876d357820cce20fc3b682d6d0a94daba5f0a4
Sholameister <info@VolunteerCoinProject.org>
2025-07-27 11:33:15 -0500

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,13 +1,11 @@
-// firebaseConfig.js
-import firebase from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js";
-import "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore-compat.js";
-import "https://www.gstatic.com/firebasejs/9.6.10/firebase-storage-compat.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCLLrOx4jWJ1PN8xFFxNhIryx3NshADKVY",
   authDomain: "lovebutton-heaven.firebaseapp.com",
   projectId: "lovebutton-heaven",
-  storageBucket: "lvbtn-bucket.appspot.com"
+  storageBucket: "lvbtn-bucket.appspot.com",
+  messagingSenderId:1:1079456151721:web:15d2aa1171d977da8c11b8",
+  measurementId: "G-0261HYV08P"
 };
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
@@ -16,4 +14,5 @@ if (!firebase.apps.length) {
 const db = firebase.firestore();
 const storage = firebase.storage();
 
-export { db, storage };
+
+
