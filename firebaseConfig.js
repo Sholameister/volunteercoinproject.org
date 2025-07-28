6853deacfc491409515abca4b239297e4f075456
Sholameister <info@VolunteerCoinProject.org>
2025-07-27 21:28:36 -0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,16 +1,24 @@
+import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getFirestore, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLLrOx4jWJ1PN8xFFxNhIryx3NshADKVY",
+  apiKey: "AIzaSyCLLrOx4JWJ1PN8xFFxNhIryx3NshADKVY",
   authDomain: "lovebutton-heaven.firebaseapp.com",
   projectId: "lovebutton-heaven",
   storageBucket: "lvbtn-bucket.appspot.com",
-  messagingSenderId:"1079456151721",
+  messagingSenderId: "1079456151721",
   appId: "1:1079456151721:web:15d2aa1171d977da8c11b8",
   measurementId: "G-0261HYV08P"
 };
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+
+// ✅ Prevent duplicate Firebase init
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApps()[0];
 }
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
-const db = getFirestore();
+const db = getFirestore(app);
+const storage = getStorage(app);
