b201994afab036f7930389f4cdc7bc9d02c5e23a
Sholameister <info@VolunteerCoinProject.org>
2025-09-26 13:25:50 -0500

    Create backingconfig.js

---
+++ backingconfig.js
@@ -0,0 +1,8 @@
+// Simple shared numbers you can edit quickly (units: BTC, SOL, troy oz)
+export const RESERVES = {
+  BTC_HELD: 0.00181,   // ← update this when you add BTC
+  SOL_HELD: 1.03,      // ← current SOL reserve
+  GOLD_OZ: 0,          // ← ounces of gold
+  BTC_TARGET_USD: 50000,
+  SOL_TARGET_USD: 50000,
+  GOLD_TARGET_USD: 50000
